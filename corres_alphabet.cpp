f37b4294a2d42e51323707cfba4aa6b480d218fe
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-22 14:32:57 +0530

    Create corres_alphabet.cpp

---
+++ corres_alphabet.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int N;
+char r;
+cout<<"Enter the number : "<<endl;
+cin>>N;
+r=64+N;
+cout<<"The corresponding character for the given number is : "<<r<<endl;
+return 0;
+}
