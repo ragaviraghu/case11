9043d763896544008cbf539206e4a6c7db43b67e
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-22 11:23:50 +0530

    Create ABC.cpp

---
+++ ABC.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int A,B,C;
+cout<<"Enter the values of A,B and C : "<<endl;
+cin>>A>>B>>C;
+cout<<"The result is : "<<((A*B)/C)<<endl;
+return 0;
+}
