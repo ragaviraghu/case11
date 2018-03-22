e76f6e2f643c00ca03834546182a28f27b7c283e
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-22 14:39:12 +0530

    Create alphabet.cpp

---
+++ alphabet.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int N;
+char a;
+cout<<"Enter the number : "<<endl;
+cin>>N;
+a=64+N;
+cout<<"The corresponding alphabet for the given number is : "<<a<<endl;
+return 0;
+}
